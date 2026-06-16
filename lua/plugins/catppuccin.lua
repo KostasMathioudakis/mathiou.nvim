97fc027c9d96bf347e5a7a429796361ea8a5d361
Kostas Mathioudakis <kostaschrisma@gmail.com>
2026-06-16 17:58:39 +0300

    Switched to darker bg

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -5,6 +5,13 @@ return {
     priority = 1000,
     opts = {
         flavour = "mocha",
+        color_overrides = {
+            mocha = {
+                base = "#080808",
+                mantle = "#050505",
+                crust = "#020202",
+            },
+        },
         custom_highlights = function()
             return {
                 CursorLineNr = {
